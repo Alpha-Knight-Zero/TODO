9e49268a111261f28153ff64a1963568cc1edf48
Pushkal Pandey <65271592+Alpha-Knight-Zero@users.noreply.github.com>
2021-03-28 09:19:40 +0530

    Update database.ts

--- server/database.ts
+++ server/database.ts
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  password: "root",
+  password: "root",//ENTER THE PASSWORD FOR postgres user in your system in place of root
   host: "localhost",
   port: 5432,
   database: "PERN-TODO",
