dd0cdeac7c9cdd9bbd0c56889971bb933c1659de
Alpha-Knight_Zero <pushkaldkpandey333@gmail.com>
2021-03-28 09:13:56 +0530

    full sql db syntax added for default database in podtgresql.

--- server/createDB.sql
+++ server/createDB.sql
@@ -1,4 +1,14 @@
-CREATE TABLE todolist (
+CREATE DATABASE "PERN-TODO"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_India.1252'
+    LC_CTYPE = 'English_India.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+	
+	CREATE TABLE todolist (
 	id SERIAL PRIMARY KEY,
 	description VARCHAR(288) NOT NULL,
 	timerecorded TIMESTAMPTZ DEFAULT Now()
